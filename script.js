d40ba6e7551fd2ffbc6bc552cf507778f1dc53ad
Asim Zahid <asimzahid307@gmail.com>
2024-12-11 19:55:19 +0500

    Printing details on web using DOM!

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//---------------------------------- SECTION NO 1 -------------------------------------------
 // Adding simple variables
 // let a = 10;
 // let b = 20;
@@ -13,11 +14,12 @@
 // console.log(e)
 // console.log(f)
 
+//---------------------------------- SECTION NO 2 -------------------------------------------
 // Function
 // let car = { make: "Honda", model: "Civic", year: "2007", color: "Silver" };
 
 // function guessCar(params) {
-
+    
 //     // let car = { make: "Honda", model: "Civic", year: "2007", color: "Silver" };
 
 //     if (car.make == "Honda") {
@@ -26,39 +28,61 @@
 //         console.log("No! Its a honda.")
 //     }
 // }
-
+        
 // let result = guessCar();
+//---------------------------------- SECTION NO 3 -------------------------------------------
 
 //  Creating object
 
-const person1 = {
-    fname: "John",
-    lname: "doe",
-    age: 30,
-    slogan: () => console.log("Hello fellas!"),
-    voice: () => console.log("My voice is good!"),
-};
 
-const person2 = {
-    fname: "Lara",
-    lname: "Croft",
-    age: 25,
-    slogan: function () { console.log("I love mountains!") },
-    voice: function () { console.log("I have a slow voice!") },
-};
+// const person1 = {
+//     fname: "John",
+//     lname: "doe",
+//     age: 30,
+//     slogan: () => '<p>Hello fellas!</p>',
+//     voice: () => '<p>My voice is good!</p>',
+// };
 
-console.log(person1['fname'])
-console.log(person1['lname'])
-console.log(person1['age'])
-person1.slogan()
-person1.voice()
+// const person2 = {
+//     fname: "Lara",
+//     lname: "Croft",
+//     age: 25,
+//     slogan: () => 'I love mountains!',
+//     voice: () => 'My voice is stealth!',
+// };
 
-console.log(person2.fname)
-console.log(person2.lname)
-console.log(person2.age)
-person2.slogan()
-person2.voice()
+// console.log(person1['fname'])
+// console.log(person1['lname'])
+// console.log(person1['age'])
+// person1.slogan()
+// person1.voice()
+
+// console.log(person2.fname)
+// console.log(person2.lname)
+// console.log(person2.age)
+// person2.slogan()
+// person2.voice()
 
 
 
+
+
+// document.getElementById('demo').innerHTML = person1.fname;
+// document.getElementById('1').innerHTML = person1.fname;
+// document.getElementById('2').innerHTML = person1.lname;
+// document.getElementById('3').innerHTML = person1.age;
+// document.getElementById('4').innerHTML = person1.slogan();
+// document.getElementById('5').innerHTML = person1.voice();
+
+
+// document.getElementById('6').innerHTML = person2.fname;
+// document.getElementById('7').innerHTML = person2.lname;
+// document.getElementById('8').innerHTML = person2.age;
+// document.getElementById('9').innerHTML = person2.slogan();
+// document.getElementById('10').innerHTML = person2.voice();
+
+
+
+//------------------------------------------- SECTION NO 4 --------------------------------------
+// now testing nested object
 
